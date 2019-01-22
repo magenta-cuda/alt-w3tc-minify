491e90a440e3588da41cde054bd44be7837461f8
Magenta Cuda <magenta.cuda@yahoo.com>
2019-01-22 08:25:06 -1000

    add GPL

--- alt-w3tc-minify.php
+++ alt-w3tc-minify.php
@@ -4,6 +4,21 @@
  * Description: record the sent order of the JavaScript files and use this to create a W3TC Minify configuration
  * Author: Magenta Cuda
  * Author URI: http://magentacuda.com
+ *
+ * Copyright (c) 2019 Magenta Cuda
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 class MC_Alt_W3TC_Minify {
