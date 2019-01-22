b7d8cb75d43d24ba9400bcc97fd6ff600e121d7f
Magenta Cuda <magenta.cuda@yahoo.com>
2019-01-21 14:28:58 -1000

    add download link for the generated conf file

--- alt-w3tc-minify.php
+++ alt-w3tc-minify.php
@@ -7,7 +7,8 @@
  */
 
 class MC_Alt_W3TC_Minify {
-    const OPTION_NAME = 'mc_alt_w3tc_minify';
+    const OPTION_NAME    = 'mc_alt_w3tc_minify';
+    const CONF_FILE_NAME = 'mc_alt_w3tc_minify.json';
     private static $theme;
     private static $basename;
     private static $files = [ 'include' => [ 'files' => [] ], 'include_footer' => [ 'files' => [] ] ];
@@ -39,6 +40,14 @@ class MC_Alt_W3TC_Minify {
                 self::update_config_file( $new_data );
             }
         } );
+        add_filter( 'plugin_action_links_alt-w3tc-minify/alt-w3tc-minify.php', function( $links ) {
+            if ( file_exists( W3TC_CONFIG_DIR . '/' . self::CONF_FILE_NAME ) ) {
+                array_push( $links,
+                    '<a href="' . WP_CONTENT_URL . '/w3tc-config/' . self::CONF_FILE_NAME . '">Download Alt W3TC Conf File</a>'
+                );
+            }
+            return $links;
+        } );
     }
     private static function update_config_file( $new_data ) {
         $config = \W3TC\Config::util_array_from_storage( 0, FALSE );
@@ -49,6 +58,14 @@ class MC_Alt_W3TC_Minify {
             }
         }
         error_log( 'MC_Alt_W3TC_Minify::update_config_file():new $config=' . print_r( $config, TRUE ) );
+        if ( defined( 'JSON_PRETTY_PRINT' ) ) {
+            $config = json_encode( $config, JSON_PRETTY_PRINT );
+        } else {  // for older php versions
+            $config = json_encode( $config );
+        }
+        $filename = W3TC_CONFIG_DIR . '/' . self::CONF_FILE_NAME;
+        $url      = WP_CONTENT_URL . '/w3tc-config/' . self::CONF_FILE_NAME;
+        \W3TC\Util_File::file_put_contents_atomic( $filename, $config );
     }
 }
 
