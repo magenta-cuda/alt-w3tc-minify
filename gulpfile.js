b86078c6bed862bbec94f9b8798f59ef5adfc830
Magenta Cuda <magenta.cuda@yahoo.com>
2019-12-11 09:34:53 -1000

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp  = require('gulp');
 var chmod = require('gulp-chmod');
 
 gulp.task('dev', function(){
-    return gulp.src(['*.php', '*.js', '*.ini', '*.txt'], {"base":"."})
+    return gulp.src(['*.php', '*.js', '*.ini', '*.txt', '!gulpfile.js', '!test-1.js'], {"base":"."})
         .pipe(chmod(0644))
         .pipe(gulp.dest('/var/www/html/wp-content/plugins/w3tc-minify-helper'))
 });
