4a599d0b63f1be185e1a923ad5308c4bd6967d7b
root <magenta.cuda@yahoo.com>
2020-09-11 07:52:32 -1000

    subclass experiment

--- version 3/w3tc-minify-helper-v3.php
+++ version 3/w3tc-minify-helper-v3.php
@@ -1,11 +1,22 @@
 <?php
 /*
  * Plugin Name: W3TC Minify Helper V3
- * Description: better JavaScript minification for W3TC's auto minify mode
+ * Description: better JavaScript minification for W3TC's auto minify mode by overloading WP_Scripts::do_item()
  * Version: 3
  */
 
+class AWM_WP_Scripts extends WP_Scripts {
+    public function do_item($handle, $group = false) {
+        $obj = $this->registered[$handle];
+        error_log('AWM_WP_Scripts::do_item():$obj->src=' . $obj->src);
+        return parent::do_item($handle, $group);
+    }
+}
+
 add_action('widgets_init', function() {
-	global $wp_scripts;
+    global $wp_scripts;
     error_log('$wp_scripts=' . print_r($wp_scripts, true));
+    if (!($wp_scripts instanceof WP_Scripts)) {
+        $wp_scripts = new AWM_WP_Scripts();
+    }
 }, 1);
